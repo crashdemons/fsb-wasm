d30d076d16099d9379b6eb5af173be4a8f734d91
crashdemons <crashenator@gmail.com>
2021-03-05 05:28:36 -0600

    rebuild with LTO

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # emscripten binaries need to be in your $PATH, run "source ./emsdk_env.sh" in the emscripten installation directory to do that
 
-emcc fsb-wasm.c fsb.c -O3 -o dist/fsb.js -s MODULARIZE=1 -s 'EXPORT_NAME="createFSBModule"' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS="['_malloc', '_free']" -s WASM=1
+emcc fsb-wasm.c fsb.c -O3 -flto -o dist/fsb.js -s MODULARIZE=1 -s 'EXPORT_NAME="createFSBModule"' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS="['_malloc', '_free']" -s WASM=1
 
 if [ $? == 0 ]; then
   cat dist/fsb.js wrapper/wrapper.js > dist/fsb-wasm.js ;
